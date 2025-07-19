54e2baf2cdc4aaf4b5755eb710f38ea51c0afe39
Eram Rahmani <rahmanieram13@gmail.com>
2025-07-19 11:51:01 +0530

    Ready for deployment

--- myweb/settings.py
+++ myweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k5!0%@c2#lo)ofxed^=u31!d$l2@q0w!(ip*eyv=%1e@=e$x46
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1']
 
 
 # Application definition
@@ -125,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mysite/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
